83247799b5949f6cc95cecea6be26ab86e980d0a
danielsotopachaqtec <61627581+danielsotopachaqtec@users.noreply.github.com>
2020-03-11 20:46:13 -0500

    add challenge class

---
+++ task.js
@@ -0,0 +1,5 @@
+// 1) Escriba una iteracion o bucle que iterará de 0 a 20, para iteración verificar si el numero actual es par o impar e imprimir en consola por ejemplo ("4 es par")
+
+// 2) Escriba una iteracion o bucle que iterará de 0 a 10, para cada iteración del bucle multiplicar el numero por 9 y imprimir en consola el resultado (por ejemplo: "2 * 9 = 18").
+
+// bonus: Utilice un bucle anidado para mostrar las tablas para cada multiplicador de 1 a 10 (100 es el resultado total)
