9976429e06957b18d7cc99069e25060cb342d6cc
danielsotopachaqtec <61627581+danielsotopachaqtec@users.noreply.github.com>
2020-03-11 20:58:15 -0500

    solve exercise 2

--- task.js
+++ task.js
@@ -2,4 +2,11 @@
 
 // 2) Escriba una iteracion o bucle que iterará de 0 a 10, para cada iteración del bucle multiplicar el numero por 9 y imprimir en consola el resultado (por ejemplo: "2 * 9 = 18").
 
-// bonus: Utilice un bucle anidado para mostrar las tablas para cada multiplicador de 1 a 10 (100 es el resultado total)
+// bonus: (Tabla de multiplicar)Utilice un bucle anidado para mostrar las tablas para cada multiplicador de 1 a 10 (100 es el resultado total)
+
+let multiplicador = 9;
+
+for (var i = 0; i <= 10; i++) {
+  var result = multiplicador * i;
+  console.log(`${multiplicador * i} = ${result}`);
+}
